9bf5c1cfdfbe7988142454c103ea4c2eab0c26e8
Travis Plunk <travis.plunk@microsoft.com>
2017-07-03 13:13:35 -0700

    refactored vcBuild script into a module

---
+++ containerFiles/vcBuildTools.psm1
@@ -0,0 +1,40 @@
+Import-Module "$PSScriptRoot\dockerInstall.psm1"
+
+$cmdContents = @"
+@echo off
+call "%ProgramFiles(x86)%\Microsoft Visual C++ Build Tools\vcbuildtools.bat" x64
+set
+"@
+function Set-SdkEnv
+{
+    $cmdPath = Join-Path $env:temp -ChildPath ([System.IO.Path]::GetRandomFileName() + '.cmd')
+    Set-Content -LiteralPath $cmdPath -Value $cmdContents -Encoding Ascii 
+    try
+    {
+        &$cmdPath | Foreach-Object {
+            $cmdVar,$cmdVal=$_.split('=')
+            if($cmdVar -ne 'Platform')
+            {
+                if((Get-Item -Path env:$cmdVar -Exclude SilentlyContinue).Value -ne $cmdVal)
+                {
+                    Write-Verbose "setting machine variable $cmdVar to $cmdVal" -Verbose
+                    [System.Environment]::SetEnvironmentVariable($cmdVar,$cmdVal,[System.EnvironmentVariableTarget]::Machine)
+                }
+            }
+        }
+    }
+    finally
+    {
+        Remove-Item -LiteralPath $cmdPath -ErrorAction SilentlyContinue -Force
+    }
+}
+
+function Install-VcBuildTools
+{
+    Install-ChocolateyPackage -PackageName vcbuildtools -ArgumentList @(
+        '-ia'
+        '"/InstallSelectableItems VisualCppBuildTools_ATLMFC_SDK;VisualCppBuildTools_NETFX_SDK"'
+    )
+    Set-SdkEnv
+    Remove-Win10SdkPackageCache
+}
--- containerFiles/visualcppbuildtools/installvisualcppbuildtoolsv2.ps1
+++
@@ -1,6 +0,0 @@
-Install-ChocolateyPackage -PackageName vcbuildtools -ArgumentList @(
-    '-ia'
-    '"/InstallSelectableItems VisualCppBuildTools_ATLMFC_SDK;VisualCppBuildTools_NETFX_SDK"'
-)
-Set-SdkEnv
-Remove-Win10SdkPackageCache
