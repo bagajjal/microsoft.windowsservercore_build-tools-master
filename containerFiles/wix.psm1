66e8f2045fe733a40072bcc3c7a73efbd92da105
Travis Plunk <travis.plunk@microsoft.com>
2017-07-03 17:31:22 -0700

    Add messaging on what is happening during wix install

--- containerFiles/wix.psm1
+++ containerFiles/wix.psm1
@@ -10,12 +10,15 @@ function Install-WixZip
     {
         $null = New-Item -Path $targetRoot -ItemType Directory
     }
+    Write-Verbose "Expanding $zipPath to $binPath ..." -Verbose
     Expand-Archive -Path $zipPath -DestinationPath $binPath
     $docExpandPath = Join-Path -Path $binPath -ChildPath 'doc'
     $sdkExpandPath = Join-Path -Path $binPath -ChildPath 'sdk'
     $docTargetPath = Join-Path -Path $targetRoot -ChildPath 'doc'
     $sdkTargetPath = Join-Path -Path $targetRoot -ChildPath 'sdk'
+    Write-Verbose "Fixing folder structure ..." -Verbose
     Move-Item -Path $docExpandPath -Destination $docTargetPath
     Move-Item -Path $sdkExpandPath -Destination $sdkTargetPath
     Append-Path -path $binPath
+    Write-Verbose "Done installing WIX!"
 }
